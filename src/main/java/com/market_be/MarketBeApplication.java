56f76a725d699f6ee14626eccee67ebc5948a380
yzoo0112-byte <yzoo0112@gmail.com>
2025-09-22 16:56:28 +0900

    Initial commit

---
+++ src/main/java/com/market_be/MarketBeApplication.java
@@ -0,0 +1,13 @@
+package com.market_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market_be/MarketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
