b7ad9982d4ea28ea63b8e5e84ef805618db7ae10
eunseok-Hwang <chwolog1115@gmail.com>
2025-09-22 20:04:14 +0900

    엔티티2차 추가
    나머지 entity추가

--- src/main/java/com/market_be/entity/Posts.java
+++ src/main/java/com/market_be/entity/Posts.java
@@ -3,6 +3,10 @@ package com.market_be.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "posts")
 @Getter
@@ -19,4 +23,22 @@ public class Posts {
     @JoinColumn(name = "user_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private User user_id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime create_at;
+
+    @LastModifiedDate
+    private LocalDateTime update_at;
+
+    private Long views;
+
+    @Column(length = 50)
+    private String hashtag;
+
 }
